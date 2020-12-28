861639680135895e8f5c96535933bc15a00fda66
Alison Queiroz Soares <alison.soares@fugro.com>
2020-12-27 22:35:59 -0300

    blink

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { identity, random, mapValues, keyBy, range, map, difference, chunk, zip } from 'lodash';
+import { identity, random, mapValues, keyBy, range, map, difference, chunk, zip, head, intersection, uniq, flatMap } from 'lodash';
 import { Subject } from 'rxjs';
 import { GameStatus, TicTacToeCell } from './tic-tac-toe.model';
 
@@ -45,18 +45,18 @@ export class TicTacToeComponent {
   ];
 
   readonly gameOver$ = new Subject<GameStatus>();
+  public winComb: number[];
 
   playerMove(key: number) {
     this.ticTacToeCells[key] = 'x';
-
-    const cellsLeft = map(this.ticTacToeCells, (x, i) => !x && i).filter(identity);
-    const left = cellsLeft.length;
-
-    if (left) {
-      this.ticTacToeCells[Number(cellsLeft[random(left - 1)])] = 'o';
-    }
-
     this.checkWinner();
+
+    this.pcMove();
+  }
+
+  isPartOfWinningCombination(element: number) {
+    if (!this.winComb) { return; }
+    return this.winComb.some(x => x === element);
   }
 
   private checkWinner() {
@@ -66,10 +66,13 @@ export class TicTacToeComponent {
     if (moves >= this.n) {
       if (this.checkWinningCombination(x)) {
         this.gameOver$.next(GameStatus.PlayerWon);
+        this.winComb = this.getWinningCombination(x);
       } else if (this.checkWinningCombination(this.moves('o'))) {
         this.gameOver$.next(GameStatus.PlayerLost);
-      } else if (moves === this.size / 2) {
+        this.winComb = this.getWinningCombination(this.moves('o'));
+      } else if (moves === Math.floor(this.size / 2)) {
         this.gameOver$.next(GameStatus.Draw);
+        this.winComb = flatMap(this.matrix);
       }
     }
   }
@@ -82,7 +85,58 @@ export class TicTacToeComponent {
     return this.winningCombinations.some(x => !difference(x, moves).length);
   }
 
+  private getWinningCombination(moves: number[]) {
+    const comb = this.winningCombinations.find(x => !difference(x, moves).length);
+    return comb;
+  }
+
+  // This is a logic to make PC not so dumb when playing.
+  private pcMove() {
+    const cellsLeft = map(this.ticTacToeCells, (x, i) => !x && i)
+      .filter(identity)
+      .map(x => Number(x));
+
+    const left = cellsLeft.length;
+
+    if (left) {
+      const center = this.ticTacToeCells[this.size / 2];
+
+      if (this.n % 2 && !center) {
+        this.ticTacToeCells[this.size / 2] = 'o';
+      } else {
+        const o = this.moves('o');
+
+        if (o.length) {
+          const x = this.moves('x');
+
+          const differencesX = this.winningCombinations.map(c => difference(c, x));
+          const minX = [...differencesX].sort((a, b) => a.length - b.length);
+          const hinderPlayer = uniq(flatMap(minX.map(y => intersection(y, cellsLeft))));
+
+          const differencesO = this.winningCombinations.map(c => difference(c, o));
+          const minO = [...differencesO].sort((a, b) => a.length - b.length);
+          const seekVictory = uniq(flatMap(minO.map(y => intersection(y, cellsLeft))));
+
+          const common = intersection(hinderPlayer, seekVictory);
+          const winningMove = common.find(y => this.checkWinningCombination([...o, y]));
+
+          const nextMove = winningMove ||
+            head(common) ||
+            head(hinderPlayer) ||
+            head(seekVictory)  ||
+            cellsLeft[random(left - 1)];
+
+          this.ticTacToeCells[nextMove] = 'o';
+        } else {
+          this.ticTacToeCells[cellsLeft[random(left - 1)]] = 'o';
+        }
+      }
+    }
+    this.checkWinner();
+  }
+
   reset() {
+    this.winComb = null;
     this.gameOver$.next(null);
     Object.assign(this.ticTacToeCells, mapValues(keyBy(this.indexes), () => null));
   }
